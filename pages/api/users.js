4add51f994199da02a52efc7710f4c84153b1e58
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-22 12:13:16 +0000

    feat! create CRUD routes with firebase api

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,13 +1,73 @@
 import { db } from '../../firebase-config';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from 'firebase/firestore';
 
 const usersRef = collection(db, 'users');
 
+// @route    GET "/"
+// @desc.    Get all contacts
+// @access   Private
 export const getUsers = async () => {
   try {
     const res = await getDocs(usersRef);
     const data = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// @route    GET "/:contactID"
+// @desc.    Get single contact by contactID
+// @access   Private
+export const getOneUser = async (id) => {
+  const userDoc = doc(db, 'users', id);
+  try {
+    const res = await getDoc(userDoc);
+    const data = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// @route    POST "/"
+// @desc.    Create a contact
+// @access   Public
+export const createUser = async (data) => {
+  try {
+    await addDoc(usersRef, data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// @route    PATCH "/:contactID"
+// @desc.    Edit a contact
+// @access   Private
+export const updateUser = async (id, data) => {
+  const userDoc = doc(db, 'users', id);
+  try {
+    await updateDoc(userDoc, data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// @route    DELETE "/:contactID"
+// @desc.    Delete a user
+// @access   Private
+export const deleteUser = async (id) => {
+  const userDoc = doc(db, 'users', id);
+  try {
+    await deleteDoc(userDoc);
   } catch (error) {
     console.log(error);
   }

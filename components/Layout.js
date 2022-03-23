423cab5832afb7e34d8ee1302b59b9248529609d
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-23 15:07:47 +0000

    feat!: create layout

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Meta from './Meta';
+import Nav from './Nav';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Meta />
+      <Nav />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;

81b3a53f9e682a165ef967b8cbe452c4da67fa29
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-25 19:21:23 +0000

    feat: add Meta component

---
+++ components/Meta.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Meta() {
+  return (
+    <Head>
+      <meta name='viewport' content='initial-scale=1, width=device-width' />
+      <meta name='description' content='track your favorite tv shows' />
+      <title>Memory Game</title>
+    </Head>
+  );
+}

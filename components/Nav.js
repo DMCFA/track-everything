6f7297bd460563dc0af5cbda7a0587c4e8bd01ce
Duarte Almeida <duartemalmeida@gmail.com>
2022-03-26 17:30:30 +0000

    feat: build main nav component structure

--- components/Nav.js
+++ components/Nav.js
@@ -2,17 +2,32 @@ import styles from './../styles/Nav.module.scss';
 import { Typography, ButtonGroup, Button } from '@mui/material';
 
 const Nav = () => {
+  const isLoggedIn = true;
+
   return (
     <div className={styles.container}>
       <Typography align='left' className={styles.title}>
         track everything
       </Typography>
-      <ButtonGroup className={styles['btn-grp']}>
-        <Button className={[styles['signup-btn'], styles.btn].join(' ')}>
-          sign up
-        </Button>
-        <Button className={styles.btn}>login</Button>
-      </ButtonGroup>
+      {isLoggedIn ? (
+        <ButtonGroup className={styles['btn-grp']}>
+          <Button variant='contained' className={styles.btn}>
+            your account
+          </Button>
+          <Button variant='contained' className={styles.btn}>
+            logout
+          </Button>
+        </ButtonGroup>
+      ) : (
+        <ButtonGroup className={styles['btn-grp']}>
+          <Button className={[styles['signup-btn'], styles.btn].join(' ')}>
+            sign up
+          </Button>
+          <Button variant='contained' className={styles.btn}>
+            login
+          </Button>
+        </ButtonGroup>
+      )}
     </div>
   );
 };

30bba0ae26b8cd8d9a2fd998928a459910e6ee0b
Duarte Almeida <duartemalmeida@gmail.com>
2022-04-02 09:40:50 +0100

    feat: recreate grid and render popular shows from props

--- components/Hello.js
+++ components/Hello.js
@@ -1,40 +1,26 @@
 import styles from './../styles/Hello.module.scss';
-import { Typography, Grid } from '@mui/material';
+import { Typography } from '@mui/material';
 
-const Hello = () => {
+const Hello = ({ shows }) => {
+  const showImages = shows.slice(0, 8).map((item) => {
+    return (
+      <div className={styles.card} key={item.id}>
+        <h4 className={styles.name}>{item.name}</h4>
+        <img
+          src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+          alt='poster image'
+          height='280px'
+          width='250px'
+        />
+      </div>
+    );
+  });
   return (
     <div className={styles.container}>
-      <Grid container className={styles.grid}>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-      </Grid>
       <Typography align='center' className={styles.title} variant='h1'>
         track all your tv shows in one place
       </Typography>
-      <Grid container className={styles.grid}>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-        <Grid className={styles.item} item xs>
-          test
-        </Grid>
-      </Grid>
+      <div className={styles.grid}>{showImages}</div>
     </div>
   );
 };

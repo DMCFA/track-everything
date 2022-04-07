c96f3db8ea76d4003e9ff076771bb8743f4361ad
Duarte Almeida <duartemalmeida@gmail.com>
2022-04-07 19:39:05 +0100

    feat: add newsletter, copyright and social logos to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,54 @@
+import { useState } from 'react';
+import { TextField, Button, Typography } from '@mui/material';
 import styles from './../styles/Footer.module.scss';
 
 const Footer = () => {
-  return <div className={styles.container}></div>;
+  const [newsletterRequested, setNewsletterRequest] = useState(false);
+
+  const newsletter = (
+    <div className={styles.newsletter}>
+      <TextField
+        id='newsletter'
+        size='small'
+        label='Sign up for our Newsletter'
+        color='secondary'
+        variant='outlined'
+        type='email'
+        // onChange={(e) => validateEmail(e.target.value)}
+      />
+      <Button
+        className={styles.button}
+        color='secondary'
+        size='large'
+        onClick={() => setNewsletterRequest(true)}
+      >
+        subscribe
+      </Button>
+    </div>
+  );
+
+  const newsletterSubmited = (
+    <div className={styles.newsletter}>
+      <Typography className={styles.success}>
+        thank you for subscribing to our newsletter!
+      </Typography>
+    </div>
+  );
+  return (
+    <div className={styles.container}>
+      {newsletterRequested ? newsletterSubmited : newsletter}
+      <div className={styles.copyright}>
+        © {new Date().getFullYear()} track everything{' '}
+      </div>
+      <div className={styles.social}>
+        <img src='/facebook.png' alt='facebook logo'></img>
+        <img src='/instagram.png' alt='instagram logo'></img>
+        <img src='/linkedin.png' alt='linkedin logo'></img>
+        <img src='/youtube.png' alt='youtube logo'></img>
+        <img src='/github.png' alt='github logo'></img>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

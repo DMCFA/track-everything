043353d370d0c9ba38883c7caea5a772da79f7ee
Duarte Almeida <duartemalmeida@gmail.com>
2022-05-01 12:53:54 +0100

    feat: add signup workflow

--- pages/signup.js
+++ pages/signup.js
@@ -1,37 +1,38 @@
 import styles from './../styles/signup.module.scss';
 import { FormGroup, Input, Button } from '@mui/material';
+import { useState } from 'react';
+import { createUser } from './api/users';
 
 const SignUp = () => {
-  const handleChange = (e) => console.log(e.target.value);
-  const handleSubmit = (e) => console.log(e);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = () => {
+    createUser(email, password);
+    setEmail('');
+    setPassword('');
+  };
   return (
     <div className={styles.container}>
       <div className={styles.title}>create your account with us</div>
       <form onSubmit={handleSubmit}>
         <FormGroup className={styles['input-group']}>
           <Input
-            type='text'
-            color='secondary'
-            autoFocus
-            required
-            placeholder='Choose your username'
-            onChange={handleChange}
-          />
-          <Input
             type='email'
             color='secondary'
             autoFocus
             required
             placeholder='Tell us your email'
-            onChange={handleChange}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             type='password'
             color='secondary'
             required
             placeholder='Pick your password'
+            onChange={(e) => setPassword(e.target.value)}
           />
           <Button
+            type='submit'
             color='secondary'
             size='large'
             style={{ fontSize: '1.3rem' }}

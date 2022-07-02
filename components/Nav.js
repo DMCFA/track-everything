b1ca6a7c9e75f0434f5b2f913e20f1402e9f2323
Duarte Almeida <duartemalmeida@gmail.com>
2022-07-02 11:27:45 +0100

    add get current signed in user functionality

--- components/Nav.js
+++ components/Nav.js
@@ -2,10 +2,12 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import styles from './../styles/Nav.module.scss';
 import { Typography, ButtonGroup, Button } from '@mui/material';
+import { loggedInUser } from '../pages/api/context';
 import { useContext } from 'react';
 
 const Nav = () => {
   const router = useRouter();
+  const user = useContext(loggedInUser);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -29,36 +31,39 @@ const Nav = () => {
           track everything
         </Typography>
       </Link>
-      <ButtonGroup className={styles['btn-grp']}>
-        <Button variant='contained' className={styles.btn}>
-          my shows
-        </Button>
-        <Button variant='contained' className={styles.btn}>
-          your account
-        </Button>
-        <Button
-          variant='contained'
-          className={styles.btn}
-          onClick={handleLogout}
-        >
-          logout
-        </Button>
-      </ButtonGroup>
-      <ButtonGroup className={styles['btn-grp']}>
-        <Button
-          className={[styles['signup-btn'], styles.btn].join(' ')}
-          onClick={handleSignUp}
-        >
-          sign up
-        </Button>
-        <Button
-          variant='contained'
-          className={styles.btn}
-          onClick={handleLogin}
-        >
-          login
-        </Button>
-      </ButtonGroup>
+      {user != null ? (
+        <ButtonGroup className={styles['btn-grp']}>
+          <Button variant='contained' className={styles.btn}>
+            my shows
+          </Button>
+          <Button variant='contained' className={styles.btn}>
+            {`${user.email} Account`}
+          </Button>
+          <Button
+            variant='contained'
+            className={styles.btn}
+            onClick={handleLogout}
+          >
+            logout
+          </Button>
+        </ButtonGroup>
+      ) : (
+        <ButtonGroup className={styles['btn-grp']}>
+          <Button
+            className={[styles['signup-btn'], styles.btn].join(' ')}
+            onClick={handleSignUp}
+          >
+            sign up
+          </Button>
+          <Button
+            variant='contained'
+            className={styles.btn}
+            onClick={handleLogin}
+          >
+            login
+          </Button>
+        </ButtonGroup>
+      )}
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
 import Layout from './../components/Layout';
 import '../styles/globals.scss';
-import React, { createContext, useState } from 'react';
-import { auth, onAuthStateChanged } from 'firebase/auth';
+import React, { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { loggedInUser } from './api/context';
+import { auth } from '../firebase-config';
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    console.log(currentUser);
+    setUser(currentUser);
+  });
+
   return (
     <React.StrictMode>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <loggedInUser.Provider value={user}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </loggedInUser.Provider>
     </React.StrictMode>
   );
 }

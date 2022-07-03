49e97307cc420e5dd75c370601891c998cb36e69
Duarte Almeida <duartemalmeida@gmail.com>
2022-07-03 14:51:35 +0100

    fix sign up functionality

--- pages/signup.js
+++ pages/signup.js
@@ -2,19 +2,29 @@ import styles from './../styles/signup.module.scss';
 import { FormGroup, Input, Button } from '@mui/material';
 import { useState } from 'react';
 import { createUser } from './api/users';
+import { useRouter } from 'next/router';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const handleSubmit = () => {
-    createUser(email, password);
-    setEmail('');
-    setPassword('');
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createUser(email, password);
+      setEmail('');
+      setPassword('');
+      router.push('/');
+    } catch (error) {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    }
   };
   return (
     <div className={styles.container}>
       <div className={styles.title}>create your account with us</div>
-      <form onSubmit={handleSubmit}>
+      <form>
         <FormGroup className={styles['input-group']}>
           <Input
             type='email'
@@ -37,6 +47,7 @@ const SignUp = () => {
             size='large'
             style={{ fontSize: '1.3rem' }}
             className={styles.button}
+            onClick={(e) => handleSubmit(e)}
           >
             sign up
           </Button>

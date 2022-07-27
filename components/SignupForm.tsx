93094884db1e6ed6327040b06ce6cdd712aeb5ae
Duarte Almeida <duartemalmeida@gmail.com>
2022-07-27 18:54:06 +0100

    add sign up form

---
+++ components/SignupForm.tsx
@@ -0,0 +1,39 @@
+// interface SignUpProps {
+//   username: string;
+//   email: string;
+//   password: string;
+// }
+
+const SignupForm = (): JSX.Element => {
+  return (
+    <div className='signup'>
+      <div className='signup__container'>
+        <form action='' method='post'>
+          <div className='signup__username'>
+            <label htmlFor=''>Username</label>
+            <input type='text' />
+            <span className='validation-username'></span>
+          </div>
+          <div className='signup__email'>
+            <label htmlFor=''>Email</label>
+            <input type='email' name='' id='' />
+            <span className='validation-email'></span>
+          </div>
+          <div className='signup__password'>
+            <label htmlFor=''>Password</label>
+            <input type='password' name='' id='' />
+            <span className='validation-password'></span>
+          </div>
+          <div className='signup__buttons'>
+            <button type='submit'>Register</button>
+            <a href='\' className='signup__login'>
+              Login
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;

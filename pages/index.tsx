2c5b63cd71a786582b978ac6b44fab4c3de7070a
Duarte Almeida <duartemalmeida@gmail.com>
2022-09-25 18:50:53 +0100

    add spacer and three columns snippet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,49 @@
 import type { NextPage } from 'next';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Nav from '../components/Nav';
+import ImageBlock from '../components/Snippets/ImageBlock';
+import ParagraphWithButton from '../components/Snippets/ParagraphWithButton';
+import Spacer from '../components/Spacer';
+import { getPopularShows } from './api/shows';
+import { useMediaQuery } from '../ts/HelperFunctions';
+import ThreeCol from '../components/Snippets/ThreeCol';
 
-const Home: NextPage = () => (
-  <>
-    <Head>
-      <title>Track TV</title>
-      <meta name='description' content='Track all your TV shows' />
-      <link rel='icon' href='/logo.png' />
-    </Head>
+const Home: NextPage = () => {
+  const [shows, setShows] = useState([]);
+  const isMobile = useMediaQuery(991);
 
-    <main />
+  useEffect(() => {
+    getPopularShows().then((results) => setShows(results));
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>Track TV</title>
+        <meta name='description' content='Track all your TV shows' />
+        <link rel='icon' href='/logo.png' />
+      </Head>
+      <header>
+        <Nav />
+      </header>
 
-    <footer />
-  </>
-);
+      <main>
+        <div className='homepage__main-container'>
+          <Spacer height={isMobile ? '2rem' : '8rem'} />
+          <div className='homepage__above-container'>
+            <ParagraphWithButton />
+            <ImageBlock shows={shows} />
+          </div>
+          <Spacer height={isMobile ? '4rem' : '3rem'} />
+          <div className='homepage__lower-container'>
+            <ThreeCol />
+          </div>
+        </div>
+      </main>
+
+      <footer />
+    </>
+  );
+};
 
 export default Home;

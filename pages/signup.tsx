4df13cfefa11de055c910079472b7f265fd1fee7
Duarte Almeida <duartemalmeida@gmail.com>
2022-10-10 19:46:05 +0100

    remove space

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,12 +1,6 @@
 import type { NextPage } from 'next';
 import SignupForm from '../components/SignupForm';
-import Spacer from '../components/Spacer';
 
-const Signup: NextPage = () => (
-  <>
-    <Spacer height="10rem" />
-    <SignupForm />
-  </>
-);
+const Signup: NextPage = () => <SignupForm />;
 
 export default Signup;

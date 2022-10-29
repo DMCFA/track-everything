58b0db329fed4445f78924f7109061469b3bc69a
Duarte Almeida <duartemalmeida@gmail.com>
2022-10-29 18:44:27 +0100

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@ import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Nav from '../components/Nav';
-import ImageBlock from '../components/Snippets/ImageBlock';
+import { ImageBlock } from '../components/Snippets/ImageBlock';
 import ParagraphWithButton from '../components/Snippets/ParagraphWithButton';
 import Spacer from '../components/Spacer';
 import { getPopularShows } from './api/shows';
 import { useMediaQuery } from '../ts/HelperFunctions';
 import ThreeCol from '../components/Snippets/ThreeCol';
+import Footer from '../components/Footer';
 
 const Home: NextPage = () => {
   const [shows, setShows] = useState([]);
@@ -29,19 +30,22 @@ const Home: NextPage = () => {
 
       <main>
         <div className='homepage__main-container'>
-          <Spacer height={isMobile ? '2rem' : '8rem'} />
+          <Spacer height={isMobile ? '3.5rem' : '12rem'} />
           <div className='homepage__above-container'>
             <ParagraphWithButton />
             <ImageBlock shows={shows} />
           </div>
-          <Spacer height={isMobile ? '4rem' : '3rem'} />
+          <Spacer height={isMobile ? '7rem' : '10rem'} />
           <div className='homepage__lower-container'>
             <ThreeCol />
           </div>
+          <Spacer height={isMobile ? '7rem' : '12rem'} />
         </div>
       </main>
 
-      <footer />
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };

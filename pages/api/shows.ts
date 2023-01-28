c04ac3ddc99faf3947c3959c7bdf900211890c77
Duarte Almeida <duartemalmeida@gmail.com>
2023-01-28 16:25:47 +0000

    interfaces

--- pages/api/shows.ts
+++ pages/api/shows.ts
@@ -5,11 +5,20 @@ export const imageURL = 'https://image.tmdb.org/t/p/original';
 const key = process.env.tmdbApiKey;
 
 export const getPopularShows = async (): Promise<any> => {
-  // try {
-  const res = await axios.get(`${baseURL}popular?api_key=${key}`);
-  const data = res.data.results;
-  return data;
-  // } catch (error) {
-  //     console.log(error);
-  // }
+  try {
+    const res = await axios.get(`${baseURL}popular?api_key=${key}`);
+    const data = res.data.results;
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getShowById = async (id: number): Promise<any> => {
+  try {
+    const res = await axios.get(`${baseURL}/${id}?api_key=${key}`);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
 };
